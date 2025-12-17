d78cab5939855a32116e0e5721deec34d16f299d
bedjra <armelbedjra12@gmail.com>
2025-12-17 09:26:34 +0000

    Finale VRAI

--- src/pages/CommandeDetail.tsx
+++ src/pages/CommandeDetail.tsx
@@ -307,7 +307,7 @@ export default function CommandeDetail() {
               )}
             </div>
 
-            {/* Reliquat */}
+            {/* Reliquat 
             <div>
               <label className="block text-sm font-medium text-gray-700 dark:text-gray-200 mb-1">
                 Reliquat (optionnel)
@@ -326,6 +326,7 @@ export default function CommandeDetail() {
                 </div>
               )}
             </div>
+            */}
 
             {/* Statut */}
             <div>
